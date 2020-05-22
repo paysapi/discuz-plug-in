a197c394b10b39e9c7d918bb6dacd05d57b6129a
z <z@z.com>
2020-05-22 11:19:52 +0800

    第一次入库

---
+++ README.md
@@ -0,0 +1,21 @@
+# discuz-plug-in
+ 
+## 上传模块
+
+将代码里的插件文件夹 `source/plugin/paysapi_alipay`,`source/plugin/paysapi_alipay_nophone`,`source/plugin/paysapi_wechat` 拷贝到 *Discuz!* 下的 `source/plugin` 中
+
+## 安装模块
+
+找到「*Discuz!* 管理中心」> 「应用」> 「插件」> 「paysapi_alipay支付」或「paysapi_alipay_nophone支付」或「paysapi_wechat支付」
+
+1. 点击 `安装`  
+2. 点击 `启用`
+3. 点击 `设置`，填上 paysapi的 `Uid` `Token`
+4. 点击 `提交`
+
+
+------
+
+
+
+
---
+++ source/plugin/paysapi_alipay/paysapi_alipay.inc.php
@@ -0,0 +1,60 @@
+<?php
+if (!defined('IN_DISCUZ')) {
+    exit('Access Denied');
+}
+
+$paysapi = $_G['cache']['plugin']['paysapi_alipay'];
+
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : '';
+
+if (!$action || $_SERVER['REQUEST_METHOD'] != 'POST') {
+    return;
+}
+
+$money = $_POST['money'];
+if (!$money || $money < 0) {
+    echo json_encode(array('code' => 400, 'msg' => '请输入正确的金额'));
+    return;
+}
+
+
+function add_order($order_id,$money)
+{
+    global $_G, $paysapi;
+    $data = array(
+        'orderid' => $order_id,
+        'status' => 1,
+        'uid' => $_G['uid'],
+        'amount' => $money  * $paysapi['integral_proportion'],
+        'price' => $money ,
+        'submitdate' => time(),
+        'ip' => $_SERVER['REMOTE_ADDR'],
+    );
+
+    C::t('forum_order')->insert($data);
+}
+
+$order_id = date('YmdHis') . str_pad(mt_rand(1, 999999), 6, '0', STR_PAD_LEFT);
+
+$notify_url = trim($_G['siteurl'] . 'source/plugin/paysapi_alipay/notify.php');
+$return_url = trim($_G['siteurl'] . 'home.php?mod=spacecp&ac=credit&op=base');
+
+$istype = 1;
+$price = $money;
+$uid=  $paysapi['Uid'];
+$token = $paysapi['Token'];
+$goodsname = 'WP-' . $order_id;
+$orderuid = "";
+$key = md5($goodsname . $istype . $notify_url . $order_id . $orderuid . $price . $return_url . $token . $uid);
+$url = "https://pay.bearsoftware.net.cn?key="
+        .$key."&notify_url=".urlencode($notify_url)
+        ."&orderid=".$order_id
+        ."&orderuid=".$orderuid
+        ."&return_url=".urlencode($return_url)
+        ."&goodsname=".$goodsname
+        ."&istype=".$istype
+        ."&uid=".$uid
+        ."&price=".$price;
+
+add_order($order_id,$money);
+echo json_encode(array('code' => 200, 'pay_url' => '$url'));
